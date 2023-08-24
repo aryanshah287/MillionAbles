c34b467297da1e926a1a20075e155d6ad47970d1
aryanshah287 <99818273+aryanshah287@users.noreply.github.com>
2023-08-24 14:45:13 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,8 +2,8 @@ import "./App.css";
 import Login from "./screens/Login";
 import SignUp from "./screens/SignUp";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import ObjectDetection from "./screens/ObjectDetection";
-import SignDetection from "./screens/SignDetection";
+//import ObjectDetection from "./screens/ObjectDetection";
+//import SignDetection from "./screens/SignDetection";
 import SpeechToText from "./screens/SpeechToText";
 import TextToSpeech from "./screens/TextToSpeech";
 import { Home } from './screens/Home.jsx'
@@ -16,12 +16,12 @@ function App() {
       <Router>
         <Routes >
           <Route path="/" element={<Home />} />   
-          <Route path="/visually_impaired" element={<VisuallyImpaired />} />   
-          <Route path="/deaf_and_dumb" element={<DeafDumb />} />   
-          <Route path="/dyslexia" element={<Dyslexia />} />   
+          //<Route path="/visually_impaired" element={<VisuallyImpaired />} />   
+          //<Route path="/deaf_and_dumb" element={<DeafDumb />} />   
+          //<Route path="/dyslexia" element={<Dyslexia />} />   
           <Route path="/speech_to_text" element={<SpeechToText />} />   
           <Route path="/text_to_speech" element={<TextToSpeech />} />   
-          <Route path="/object_detection" element={<ObjectDetection />} />     
+          //<Route path="/object_detection" element={<ObjectDetection />} />     
         </Routes>
       </Router>
     </div>
